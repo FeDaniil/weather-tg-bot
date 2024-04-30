638d2ae3c8c00266947e024077d784c8de1b0302
Fedorov Daniil <fedaniil@mail.ru>
2024-04-30 13:52:21 +0300

    Add test plotly image send

--- Dockerfile
+++ Dockerfile
@@ -1,23 +1,24 @@
-FROM python:3.10.13-alpine3.18
+FROM python:3.12-bookworm
 
 ENV POETRY_NO_INTERACTION=1 \
     POETRY_VIRTUALENVS_IN_PROJECT=1 \
     POETRY_VIRTUALENVS_CREATE=0 \
     POETRY_HOME="/etc/poetry" \
     POETRY_CACHE_DIR="/tmp/poetry_cache" \
-    POETRY_VERSION=1.7.0
+    POETRY_VERSION=1.8.2
 
 WORKDIR /usr/src/app
 
 COPY . .
 
 RUN pip install --no-cache-dir "poetry==$POETRY_VERSION" \
+    && poetry lock \
     && poetry install --without admin --without dev --no-root \
     && pip uninstall -y poetry \
     && pybabel compile -d bot/locales \
     && rm -rf /home/appuser/.cache \
     && rm -rf $POETRY_CACHE_DIR \
-    && adduser -D appuser \
+    && adduser --disabled-login appuser \
     && chown -R appuser:appuser .
 
 USER appuser
--- bot/handlers/info.py
+++ bot/handlers/info.py
@@ -1,6 +1,9 @@
 from aiogram import Router, types
 from aiogram.filters import Command
 from aiogram.utils.i18n import gettext as _
+from aiogram.types import BufferedInputFile
+
+import plotly.express as px
 
 router = Router(name="info")
 
@@ -8,4 +11,10 @@ router = Router(name="info")
 @router.message(Command(commands=["info", "help", "about"]))
 async def info_handler(message: types.Message) -> None:
     """Information about bot."""
-    await message.answer(_("about"))
+    df = px.data.wind()
+    fig = px.bar_polar(df, r="frequency", theta="direction",
+                   color="strength", template="plotly_dark",
+                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+    img_bytes = fig.to_image(format="png")
+    # test reply with image
+    await message.answer_photo(BufferedInputFile(img_bytes, filename="plot.png"), caption=_("about"))
