a392d221aaece2518295d3559ff61a87704668a4
Fedorov Daniil <fedaniil@mail.ru>
2024-04-30 14:35:48 +0300

    Add response to keyboard button

--- bot/handlers/info.py
+++ bot/handlers/info.py
@@ -1,5 +1,7 @@
+from aiogram import F
 from aiogram import Router, types
 from aiogram.filters import Command
+from aiogram.filters.magic_data import MagicData
 from aiogram.utils.i18n import gettext as _
 from aiogram.types import BufferedInputFile
 
@@ -18,3 +20,14 @@ async def info_handler(message: types.Message) -> None:
     img_bytes = fig.to_image(format="png")
     # test reply with image
     await message.answer_photo(BufferedInputFile(img_bytes, filename="plot.png"), caption=_("about"))
+
+@router.callback_query(MagicData(F.event.data == "info"))
+async def info_cb_handler(query: types.CallbackQuery) -> None:
+    """Information about bot."""
+    df = px.data.wind()
+    fig = px.bar_polar(df, r="frequency", theta="direction",
+                   color="strength", template="plotly_dark",
+                   color_discrete_sequence= px.colors.sequential.Plasma_r)
+    img_bytes = fig.to_image(format="png")
+    # test reply with image
+    await query.message.answer_photo(BufferedInputFile(img_bytes, filename="plot.png"), caption=_("about"))
